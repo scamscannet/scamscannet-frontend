ade4b914e7ee923161dc54f0cab94fd57f3b528f
Felix <62290842+ctrl-Felix@users.noreply.github.com>
2023-01-10 22:34:49 +0100

    change icon for blacklisted domain

--- src/routes/tools/blacklist/[domain]/+page.svelte
+++ src/routes/tools/blacklist/[domain]/+page.svelte
@@ -65,8 +65,13 @@
 
     <div class="flex flex-col items-center border py-6">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class:text-yellow-400={!analysis.isFraud} class:text-red-500={analysis.isFraud} class="w-36 h-36">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
-        </svg>
+            {#if !analysis.isFraud}
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+            {:else }
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+            {/if}
+                </svg>
+
 
         <h2 class="font-bold text-3xl text-center">{analysis.domain.toUpperCase()}<br> is {analysis.isFraud ? '' : 'not'} blacklisted</h2>
 
