50a6811af758761fb28683ccf2e52a2484eb32a6
Felix <62290842+ctrl-Felix@users.noreply.github.com>
2023-01-11 11:06:28 +0100

    display parsed whois request

--- src/routes/tools/whois/[domain]/+page.svelte
+++ src/routes/tools/whois/[domain]/+page.svelte
@@ -1,10 +1,19 @@
 <script>
     import {onMount} from "svelte";
-    import {CLIENTS, makeApiRequest} from "../../../../lib/client/request";
-    import {APIS, PATHS} from "../../../../lib/client/apis";
+    import {CLIENTS, makeApiRequest} from "$lib/client/request";
+    import {APIS, PATHS} from "$lib/client/apis";
     import {page} from "$app/stores";
     import LoadingView from "../../../_components/LoadingView.svelte";
-    import {Accordion, AccordionItem, Breadcrumb, BreadcrumbItem, Modal} from "flowbite-svelte";
+    import {
+        Accordion,
+        AccordionItem,
+        Breadcrumb,
+        BreadcrumbItem,
+        Card,
+        Modal, Table, TableBody,
+        TableBodyCell,
+        TableBodyRow, TableHead, TableHeadCell
+    } from "flowbite-svelte";
 
     let result = null;
 
@@ -17,10 +26,13 @@
             {}
         ).then(response => {
             result = response.content;
-            console.log(result.raw.split("\n"))
         })
 
     })
+
+    function capitalize(string){
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
 </script>
 <Breadcrumb aria-label="Default breadcrumb example" solid>
     <BreadcrumbItem href="/" home>Home</BreadcrumbItem>
@@ -33,10 +45,62 @@
 {#if (!result)}
     <LoadingView></LoadingView>
 {:else}
+    {#if result.parsed.domain}
+        <div class="grid grid-cols-4 mb-4 gap-4">
+            <Card>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Registrar</h5>
+                <a target="_blank" href="{result.parsed.registrar.url}" class="font-normal text-lg text-gray-700 dark:text-gray-400 leading-tight">
+                    {result.parsed.registrar.name}
+                </a>
+            </Card>
+            <Card>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Registration Date</h5>
+                <p class="font-normal text-lg text-gray-700 dark:text-gray-400 leading-tight">
+                    {new Date(Date.parse(result.parsed.date.registration_date)).toLocaleDateString()}
+                </p>
+            </Card>
+            <Card>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Expiration Date</h5>
+                <p class="font-normal text-lg text-gray-700 dark:text-gray-400 leading-tight">
+                    {result.parsed.date.expiration_date ? new Date(Date.parse(result.parsed.date.expiration_date)).toLocaleDateString() : "No expiration"}
+                </p>
+            </Card>
+            <Card>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Abuse Contact</h5>
+                <a href="mailto:{result.parsed.registrar.abuse_mail}" class="font-normal text-lg text-gray-700 dark:text-gray-400 leading-tight">
+                    {result.parsed.registrar.abuse_mail}
+                </a>
+            </Card>
+        </div>
+        {#each result.parsed.contact as contact}
+        <div class="border overflow-hidden rounded-lg mb-4">
+                <Table striped={true}>
+                    <TableHead>
+                        <TableHeadCell>{contact.type}</TableHeadCell>
+                        <TableHeadCell>Contact</TableHeadCell>
+                    </TableHead>
+                    <TableBody class="divide-y">
+                        {#each Object.entries(contact) as [title, value] }
+                            {#if title && title !== "type" && value}
+                                <TableBodyRow>
+                                    <TableBodyCell>{capitalize(title).replace("-", "")}</TableBodyCell>
+                                    <TableBodyCell>{value}</TableBodyCell>
+                                </TableBodyRow>
+                            {/if}
+                        {/each}
+                    </TableBody>
+                </Table>
+            </div>
+        {/each}
+
+
+
+
+    {/if}
     <Accordion>
         <AccordionItem open>
             <span slot="header">Raw Whois Response</span>
-            <p class="mb-2 text-gray-500 dark:text-gray-400">
+            <p class="mb-2 text-gray-600 dark:text-gray-400">
                 {#each result.raw.split("\n") as line}
                     {line}<br />
                 {/each}
