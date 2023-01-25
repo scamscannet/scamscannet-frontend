12cf8e40b922ff23cb1b15571313557ec149914b
Felix <62290842+ctrl-Felix@users.noreply.github.com>
2023-01-25 20:13:24 +0100

    new faq at blacklist page

--- src/routes/tools/blacklist/+page.svelte
+++ src/routes/tools/blacklist/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import {Input, Button, Spinner, Progressbar, Breadcrumb, BreadcrumbItem} from "flowbite-svelte";
     import DomainInputForm from "../_components/DomainInputForm.svelte";
+    import CardView from "../../_components/CardView.svelte";
 
     let loading = {
         blacklistCheck: false
@@ -17,3 +18,9 @@
 
 
 <DomainInputForm redirectURL="/tools/blacklist/" ></DomainInputForm>
+<CardView>
+    <div class="flex flex-col mx-auto w-full md:w-4/5 xl:w-2/3">
+        <h3 class="text-xl font-bold">About the ScamScan Doman Blacklist</h3>
+        The ScamScan Domain Blacklist contains all known fraudulent domains/websites. In addition, we have trained an AI model based on our data that can predict whether a domain name appears to be fraudulent or not. Unfortunately, the blacklist is limited to subdomains so far. We are already working on an extension. Our blacklist is already successfully used in the Keplr cryptocurrency wallet to protect users from fraud. If you would like to integrate the blacklist, you can contact us at any time.
+    </div>
+</CardView>
