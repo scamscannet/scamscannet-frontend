35a533833c3960e355aee04018a936b64b9acc63
Felix <62290842+ctrl-Felix@users.noreply.github.com>
2023-01-26 18:49:29 +0100

    improved blacklist card

--- src/routes/check/[domain]/+page.svelte
+++ src/routes/check/[domain]/+page.svelte
@@ -29,7 +29,7 @@ import InfoCard from "../_components/InfoCard.svelte";
         <div class="flex justify-center rounded-lg border border-lg w-full h-full py-4 mt-4 px-4">
             <div class="w-full h-full flex flex-col">
                 <h3 class="text-xl font-bold mb-4">Blacklist</h3>
-                <div class="w-36 h-36 flex flex-col items-center">
+                <div class="w-full h-36 flex flex-col items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-36 h-36 text-yellow-400">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
                     </svg>
@@ -95,12 +95,12 @@ import InfoCard from "../_components/InfoCard.svelte";
 <CardView>
     <div class="flex flex-col mx-auto w-full ">
         <h3 class="text-xl font-bold">FAQ</h3>
-        <p class="font-bold mt-2">What is all of this information telling me?</p>
+        <h2 class="font-bold mt-2">What is all of this information telling me?</h2>
         <p>All the information here will help you to get an overall picture of {$page.params.domain}. If you hover with the mouse cursor over the respective fields it will also explain in more detail how you can use the information.</p>
         <div class="border w-3/4 mx-auto my-6"></div>
         <p class="font-bold mt-2">I want to report a fraudulent domain!</p>
         <p>We are always happy about reports of fraudulent domains as these are an important source for our blacklist. If you want to report {$page.params.domain} or any other website you can use the form on the home page to report scam domains to us! It does not matter if the displayed scam score is high or low.</p>
-        <p class="font-bold mt-2">Who runs {$page.params.domain}</p>
+        <h2 class="font-bold mt-2">Who runs {$page.params.domain}</h2>
         <p>{$page.params.domain} has been registered with REGISTRAR HERE on DATE HERE. If you want to know more about the owner you can have a look at the Whis request for the page. Due to privacy many people redacted the information in their Whois record which makes it very hard to be able to identify the real owner.</p>
 
 
