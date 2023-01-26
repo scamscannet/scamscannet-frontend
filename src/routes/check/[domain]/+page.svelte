747fa514b39f3e1c37124b4d712a9be247bda593
Felix <62290842+ctrl-Felix@users.noreply.github.com>
2023-01-26 18:08:58 +0100

    scam check page preview

--- src/routes/check/[domain]/+page.svelte
+++ src/routes/check/[domain]/+page.svelte
@@ -6,38 +6,44 @@ import {Button, Card} from "flowbite-svelte";
 </script>
 
 <h1 class="font-bold text-2xl">Is {$page.params.domain} a scam?</h1>
-<CardView>
-    <div class="w-full">
-        <h3 class="text-xl font-bold">AI Ratings</h3>
-        <p>We developed AIs to automatically analyse gathered information to determine whether a domain is scam or not. These scores are useful in addition to the overall picture but you shouldn't base your whole judgment on these two scores.</p>
-        <h3 class="mt-4 font-bold">Web Page Analysis</h3>
-        <div class="w-full bg-gray-200 rounded-full">
-            <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-2 leading-none rounded-full" style="width: 25%"> 25%</div>
-        </div>
-        <h3 class="mt-2 font-bold">Domain Name</h3>
-        <div class="w-full bg-gray-200 rounded-full">
-            <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-2 leading-none rounded-full" style="width: 25%"> 25%</div>
-        </div>
+<div class="grid grid-cols-1 sm:grid-cols-4 gap-4 pb-4">
+    <div class="sm:col-span-3">
+        <div class="flex justify-center rounded-lg border border-lg w-full h-full py-4 mt-4 px-4">
+            <div class="">
+                <h3 class="text-xl font-bold">AI Ratings</h3>
+                <p>We developed AIs to automatically analyse gathered information to determine whether a domain is scam or not. These scores are useful in addition to the overall picture but you shouldn't base your whole judgment on these two scores.</p>
+                <h3 class="mt-4 font-bold">Web Page Analysis</h3>
+                <div class="w-full bg-gray-200 rounded-full">
+                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-2 leading-none rounded-full" style="width: 25%"> 25%</div>
+                </div>
+                <h3 class="mt-2 font-bold">Domain Name</h3>
+                <div class="w-full bg-gray-200 rounded-full">
+                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-2 leading-none rounded-full" style="width: 25%"> 25%</div>
+                </div>
 
-    </div>
-</CardView>
-<CardView>
-    <div class="w-full">
-        <h3 class="text-xl font-bold mb-4">Blacklist</h3>
-        <div class="w-full flex justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-36 h-36 text-yellow-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
-            </svg>
-
+            </div>
         </div>
     </div>
+    <div class="">
+        <div class="flex justify-center rounded-lg border border-lg w-full h-full py-4 mt-4 px-4">
+            <div class="w-full">
+                <h3 class="text-xl font-bold mb-4">Blacklist</h3>
+                <div class="w-full flex justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-36 h-36 text-yellow-400">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                    </svg>
 
-</CardView>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
 <CardView>
     <div class="w-full">
         <h3 class="text-xl font-bold mb-4">Metrics</h3>
         <div class="grid grid-cols-3 gap-4">
-            <Card>
+            <Card class="">
                 <p class="text-red-700 text-lg">17.02.2022</p>
                 <p>Domain age</p>
             </Card>
@@ -83,12 +89,11 @@ import {Button, Card} from "flowbite-svelte";
 
 </CardView>
 <CardView>
-    <div class="flex flex-col mx-auto w-full md:w-4/5 xl:w-2/3">
+    <div class="flex flex-col mx-auto w-full ">
         <h3 class="text-xl font-bold">FAQ</h3>
-        <div class="border w-2/3 mx-auto my-6"></div>
         <p class="font-bold mt-2">What is all of this information telling me?</p>
-        <p>All die Informationen hier helfen sich ein Gesamtbild von {$page.params.domain} zu machen. Wenn du mit dem Maus Cursor über die jeweiligen Felder hoverst wird dir auch genauer erklärt, wie du die Information nutzen kannst.</p>
-        <div class="border w-2/3 mx-auto my-6"></div>
+        <p>All the information here will help you to get an overall picture of {$page.params.domain}. If you hover with the mouse cursor over the respective fields it will also explain in more detail how you can use the information.</p>
+        <div class="border w-3/4 mx-auto my-6"></div>
         <p class="font-bold mt-2">I want to report a fraudulent domain!</p>
         <p>We are always happy about reports of fraudulent domains as these are an important source for our blacklist. Please use the form which you can reach via the home page to report scam domains to us! It does not matter if the displayed score is high or low.</p>
 
