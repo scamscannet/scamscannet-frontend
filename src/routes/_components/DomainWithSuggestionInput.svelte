8f7b6de1f3227f461e18226a3b89b0061d4f2fb5
Felix <62290842+ctrl-Felix@users.noreply.github.com>
2023-03-01 13:13:25 +0100

    improved check page

--- src/routes/_components/DomainWithSuggestionInput.svelte
+++ src/routes/_components/DomainWithSuggestionInput.svelte
@@ -24,14 +24,14 @@
     {#if !domainInput}
         <Button class="w-64 rounded-l-none" disabled>Please enter a domain</Button>
     {:else }
-        <Button on:click={() => {domainInput = ""; window.location.replace("/check/" + domainInput)}} class="w-64 rounded-l-none">Check for scam</Button>
+        <Button on:click={() => {window.location.replace("/check/" + domainInput)}} class="w-64 rounded-l-none">Check for scam</Button>
     {/if}
 </div>
 {#if suggestions.length > 0}
     <div class='w-full w-full mt-2 text-gray-500' class:absolute={suggestionHover}>
         <div class="flex flex-col w-full rounded-lg border overflow-hidden">
             {#each suggestions as suggestion, id}
-                <button on:click={() => {domainInput = ""; window.location.replace("/check/" + suggestion.full_domain)}} class="text-left py-2 px-2 hover:bg-blue-500 hover:text-gray-50" class:bg-gray-200={id % 2} class:bg-white={!(id % 2)}>{suggestion.full_domain}</button>
+                <button on:click={() => {window.location.replace("/check/" + suggestion.full_domain)}} class="text-left py-2 px-2 hover:bg-blue-500 hover:text-gray-50" class:bg-gray-200={id % 2} class:bg-white={!(id % 2)}>{suggestion.full_domain}</button>
             {/each}
         </div>
     </div>
